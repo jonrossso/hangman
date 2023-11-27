81f2a9c3026dc60820f5a7d3a45ade873dcb7b4a
jonrossso <jayrossofficial@gmail.com>
2023-11-27 09:57:25 +0000

    New changes including milestone_4.py

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -6,6 +6,3 @@ print(fruit_word_list)
 
 randomly_selected_word_in_fruit_word_list = random.choice(fruit_word_list)
 print("Randomly selected word: ", randomly_selected_word_in_fruit_word_list)
-
-
-
